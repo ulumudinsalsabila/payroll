06d5e27394f63986335fe6b9578ca524585ba963
Ulumudin <ulumudinuwi@gmail.com>
2026-09-14 08:26:30 +0700

    Update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,31 +2,24 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
         User::updateOrCreate(
-            ['email' => 'admin@hasnautama.com'],
+            [
+                'email' => 'admin@hasnautama.com',
+            ],
             [
                 'name' => 'Super Admin',
                 'password' => Hash::make('12345678'),
                 'role' => 'HRD',
-                'timezone' => 'Asia/Jakarta'
+                'timezone' => 'Asia/Jakarta',
             ]
         );
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
     }
 }
